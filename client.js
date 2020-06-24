811bb072ac5ce947a2ee5bc55521742a84195bab
Kenneth Carter <kxccarter@gmail.com>
2020-06-24 16:55:19 -0500

    My local version. stretch goal not complete.

--- client.js
+++ client.js
@@ -1,36 +1,34 @@
-const employees = [
-  {
-    name: 'Atticus',
-    employeeNumber: '2405',
-    annualSalary: '47000',
-    reviewRating: 3
-  },
-  {
-    name: 'Jem',
-    employeeNumber: '62347',
-    annualSalary: '63500',
-    reviewRating: 4
-  },
-  {
-    name: 'Scout',
-    employeeNumber: '6243',
-    annualSalary: '74750',
-    reviewRating: 5
-  },
-  {
-    name: 'Robert',
-    employeeNumber: '26835',
-    annualSalary: '66000',
-    reviewRating: 1
-  },
-  {
-    name: 'Mayella',
-    employeeNumber: '89068',
-    annualSalary: '35000',
-    reviewRating: 1
-  }
+const employees = [{
+        name: 'Atticus',
+        employeeNumber: '2405',
+        annualSalary: '47000',
+        reviewRating: 3
+    },
+    {
+        name: 'Jem',
+        employeeNumber: '62347',
+        annualSalary: '63500',
+        reviewRating: 4
+    },
+    {
+        name: 'Scout',
+        employeeNumber: '6243',
+        annualSalary: '74750',
+        reviewRating: 5
+    },
+    {
+        name: 'Robert',
+        employeeNumber: '26835',
+        annualSalary: '66000',
+        reviewRating: 1
+    },
+    {
+        name: 'Mayella',
+        employeeNumber: '89068',
+        annualSalary: '35000',
+        reviewRating: 1
+    }
 ];
-
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
 
 // Take small steps! Don't write a for loop and two functions that do all of the calculations right away.
@@ -40,4 +38,63 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-console.log( employees );
+let list = document.getElementById('dataOut');
+
+document.onload = handleClick;
+
+function processBonuses() {
+    for (let each of employees) {
+        bonusCalc(each);
+        let empBonusData = bonusCalc(each);
+        let listNode = document.createElement("li");
+        list.appendChild(listNode);
+    }
+}
+
+
+
+
+
+function bonusCalc(emp) {
+    let employeeBonusData = {
+        name: emp.name,
+    }
+
+    let percentage;
+    let salary = parseInt(emp.annualSalary);
+
+    switch (emp.reviewRating) {
+        case 1:
+            percentage = 0;
+            break;
+        case 2:
+            percentage = 0;
+            break;
+        case 3:
+            percentage = 0.04;
+            break;
+        case 4:
+            percentage = 0.06;
+            break;
+        case 5:
+            percentage = 0.10;
+            break;
+    }
+
+    if (emp.employeeNumber.length === 4) percentage += 0.05;
+    if (salary > 65000 && percentage != 0) percentage -= 0.01;
+    if (percentage > 0.13) percentage = 0.13;
+
+    employeeBonusData.bonusPercentage = percentage;
+    employeeBonusData.totalBonus = Math.round(salary * percentage);
+    employeeBonusData.totalCompensation = salary + employeeBonusData.totalBonus;
+
+    return employeeBonusData;
+}
+
+
+
+function handleClick() {
+    document.getElementById('run').onclick = processBonuses;
+}
+//This was working because I changed the location of the script tag in index.html. I think I have to find another way to make this work, but for now I'm keeping it where it is.
